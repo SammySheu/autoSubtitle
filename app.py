8f1a64bc6087bafe63e02b7e58774081d0e7b1e1
SammySheu <samsheu1997@gmail.com>
2023-01-23 22:27:23 +0800

    Edit upload function from any files to mp4 file only

--- app.py
+++ app.py
@@ -4,31 +4,40 @@ from datetime import datetime
 import os
 
 app = Flask(__name__)
-app.config['UPLOAD_DIRECTORY'] = 'uploads/'
+app.config['UPLOAD_FOLDER'] = 'uploads/'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024     # 16 MB
+app.config['ALLOWED_EXTENSIONS'] = ['.mp4']
 
 
 @app.route('/', methods=['GET'])
 def index():
-    files = os.listdir(app.config['UPLOAD_DIRECTORY'])
-    print(files)
-    return render_template('upload_page.html', files = files)
+    files = os.listdir(app.config['UPLOAD_FOLDER'])
+    videos = []
+    for file in files:
+        if os.path.splitext(file)[1] in app.config['ALLOWED_EXTENSIONS']:
+            videos.append(file)
+    print(videos)
+    return render_template('upload_page.html', videos = videos)
 
 @app.route('/upload-video', methods=['POST'])
 def upload():
     file = request.files['file_from_user']
     if file:                # Make sure there is file included in request.file
-        time_now = datetime.now().strftime('%Y-%m-%d_%H:%M:%S_')
-        file.save(os.path.join(
-            app.config['UPLOAD_DIRECTORY'],
-            time_now + secure_filename(file.filename)
-        ))
-        return 'Upload successfully'
+        extension = file.filename.split('.')[1]
+        if extension == 'mp4':
+            time_now = datetime.now().strftime('%Y-%m-%d_%H:%M:%S_')
+            file.save(os.path.join(
+                app.config['UPLOAD_FOLDER'],
+                time_now + secure_filename(file.filename)
+            ))
+            return 'Upload successfully'
+        else:
+            return 'Only mp4 file is available'
     return 'There is no any files'
 
 @app.route('/serve-file/<filename>', methods=['GET'])
 def display_files(filename):
-    return send_from_directory(app.config['UPLOAD_DIRECTORY'], filename)
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
  
 if __name__ == "__main__":
     app.run(debug=True)
