aa3e6c6cc43b97d38416467079e0ba86e13c08d3
SammySheu <samsheu1997@gmail.com>
2023-01-23 17:26:53 +0800

    Add display_files function and serve files in upload_page.html(Jinja)

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
 from werkzeug.utils import secure_filename
 from datetime import datetime
 import os
@@ -10,7 +10,9 @@ app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024     # 16 MB
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('upload_page.html')
+    files = os.listdir(app.config['UPLOAD_DIRECTORY'])
+    print(files)
+    return render_template('upload_page.html', files = files)
 
 @app.route('/upload-video', methods=['POST'])
 def upload():
@@ -24,5 +26,9 @@ def upload():
         return 'Upload successfully'
     return 'There is no any files'
 
+@app.route('/serve-file/<filename>', methods=['GET'])
+def display_files(filename):
+    return send_from_directory(app.config['UPLOAD_DIRECTORY'], filename)
+ 
 if __name__ == "__main__":
     app.run(debug=True)
